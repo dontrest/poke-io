f5f099f36916052b04eb0a55513942c7326a9864
byulmeng <kwanchai2535@gmail.com>
2018-12-18 07:42:44 +0700

    แนททระ

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 const fs = require('fs');
-var maps = fs.readFileSync("patch/maps.json","uft-8");
-
-maps.array.forEach(map => {
+var maps = JSON.parse(fs.readFileSync("patch/maps.json","utf-8"));
+var pokemon = JSON.parse(fs.readFileSync("patch/pokemon.json" , "utf-8"));
+maps.forEach(map => {
     initMap(map);
+    console.log(map);
 });
 var last_time = new Date().getMilliseconds();
-while(true){
+/*while(true){
     let alpha = new Date().getMilliseconds()-last_time; 
     last_time = new Date().getMilliseconds();
     for(let i = 0 ; i<maps.length ;i++){
         
     }
-}
+}*/
 
 function initMap(map){
     map.enemy = [];
     map.people = 0;
     map.npc=[];
+    for(let i = 0 ; i < map.spawn.length ; i++){
+        let spawn = map.spawn[i];
+        //console.log(spawn);
+        for(let spawnnum = 0 ; spawnnum < spawn.max ;spawnnum++){
+            let enemy = {
+                "type":spawn.type,
+                "id" : spawn.id,
+                "name" : spawn.type === "pokemon"? pokemon[spawn.id].name :null
+            }
+            enemy.brain = {"event" : "wait", "duration": Math.round(Math.random()*1000+1000)}
+            enemy.status = 0;
+            enemy.x = Math.round(((Math.random()) * (spawn.area[1]-spawn.area[0])) +spawn.area[0]); 
+            enemy.y = Math.round(Math.random() * map.height);
+            map.enemy.push(enemy);
+        }    
+    }
 }
