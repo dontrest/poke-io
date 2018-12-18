3479a24e3c366e08b5df9f7b7cc444f7373dffb4
DontRest <kwanchai2535@gmail.com>
2018-12-18 17:01:08 +0700

    add web socket

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const fs = require('fs');
+var io = require('socket.io')(9999);
+
+var session_token = [{}];
+
+io.sockets.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('connection' , data=>{
+        
+    });
+
+    socket.on('choose_char' , data=>{
+        
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Got disconnect!');
+
+
+    });
+});
+
 var maps = JSON.parse(fs.readFileSync("patch/maps.json","utf-8"));
 var pokemon = JSON.parse(fs.readFileSync("patch/pokemon.json" , "utf-8"));
 maps.forEach(map => {
@@ -7,15 +28,15 @@ maps.forEach(map => {
 });
 var last_time = new Date().getMilliseconds();
 /*while(true){
-    let alpha = new Date().getMilliseconds()-last_time; 
+    let alpha = new Date().getMilliseconds()-last_time;
     last_time = new Date().getMilliseconds();
     for(let i = 0 ; i<maps.length ;i++){
-        
     }
 }*/
 
 function initMap(map){
     map.enemy = [];
+    map.player = [];
     map.people = 0;
     map.npc=[];
     for(let i = 0 ; i < map.spawn.length ; i++){
@@ -29,9 +50,11 @@ function initMap(map){
             }
             enemy.brain = {"event" : "wait", "duration": Math.round(Math.random()*1000+1000)}
             enemy.status = 0;
-            enemy.x = Math.round(((Math.random()) * (spawn.area[1]-spawn.area[0])) +spawn.area[0]); 
-            enemy.y = Math.round(Math.random() * map.height);
+            enemy.x = Math.floor(((Math.random()) * (spawn.area[1]-spawn.area[0])) +spawn.area[0]); 
+            enemy.y = Math.floor(Math.random() * map.height);
+            enemy.direction = Math.floor(Math.random() * 2);
             map.enemy.push(enemy);
-        }    
+        }
     }
 }
+
